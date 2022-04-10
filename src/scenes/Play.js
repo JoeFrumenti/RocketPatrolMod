d7f280f7ca18041c8407b3662b567b5c4a8a3eb6
Joe Frumenti <jfrument@ucsc.edu>
2022-04-10 10:50:22 -0700

    Added rocket movement. also rocket? was there a rocket in the last commit?

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -26,10 +26,16 @@ class Play extends Phaser.Scene
         this.starfield = this.add.tileSprite(0,0,640,480, 'starfield').setOrigin(0,0);
 
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update()
     {
         this.starfield.tilePositionX -=4;
+        this.p1Rocket.update();
     }
 }
