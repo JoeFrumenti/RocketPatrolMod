1635273cceea31c54883e862e2891cab6d10fc95
Joe Frumenti <jfrument@ucsc.edu>
2022-04-10 12:16:17 -0700

    added shooting and explosion sfx

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,10 @@ class Play extends Phaser.Scene
         this.load.image('spaceship','assets/spaceship.png');
         this.load.image('starfield', 'assets/starfield.png');
         this.load.spritesheet('explosion', 'assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+
+        this.load.audio('sfx_select', 'assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', 'assets/explosion38.wav');
+        this.load.audio('sfx_rocket', 'assets/rocket_shot.wav');
     }
 
     create()
@@ -137,5 +141,7 @@ class Play extends Phaser.Scene
 
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+
+        this.sound.play('sfx_explosion');
     }
 }
